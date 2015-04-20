a6de46e493985e8ad4e51bf09b4d0bf2c389138c
Reid Baldwin & Bryan Borgeson <student@epicodus.com>
2015-04-20 11:38:43 -0700

    add initial files, populate list of amazing restaurants

---
+++ js/RestaurantsController.js
@@ -0,0 +1,10 @@
+bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
+    $scope.restaurants = [
+        { name: "McDonalds" },
+        { name: "Arby's" },
+        { name: "Wendy's" },
+        { name: "Olive Garden" },
+        { name: "Stuckey's" },
+        { name: "Hooters" }
+    ]
+});
